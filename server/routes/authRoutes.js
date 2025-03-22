a93f3b76ea7ecc8b572be538fc9ed22b43acc300
Neeraj Sharma <neerajmudgal684@gmail.com>
2025-03-22 17:09:56 +0530

    Converted mongodb models format from tsx to js and fixed authroutes issue

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -4,3 +4,5 @@ const {registerUser, loginUser} = require('../controllers/authController');
 
 router.route('/signup').post(registerUser);
 router.route('/login').post(loginUser);
+
+module.exports = router;
